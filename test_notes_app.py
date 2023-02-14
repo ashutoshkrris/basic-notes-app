9d6edf2215ed76a1562f23865237b30fb9dce4db
Ashutosh Krishna <ashutoshbritish@gmail.com>
2023-02-14 17:01:26 +0530

    Add PyTest Fixtures

--- test_notes_app.py
+++ test_notes_app.py
@@ -1,4 +1,20 @@
+import pytest
+
 from notes_app import NotesApp
+
+
+@pytest.fixture
+def app_without_notes():
+    app = NotesApp()
+    return app
+
+
+@pytest.fixture
+def app_with_notes():
+    app = NotesApp()
+    app.add_note("Test note 1")
+    app.add_note("Test note 2")
+    return app
 
 
 def test_add_note():
@@ -9,28 +25,22 @@ def test_add_note():
     assert notes.notes_list[0].content == "Test note 1"
 
 
-def test_get_note():
-    notes = NotesApp()
-    notes.add_note("Test note 1")
-    result = notes.get_note(0)
+def test_get_note(app_with_notes):
+    result = app_with_notes.get_note(0)
     assert result == "Test note 1"
 
 
-def test_get_note_index_error():
-    notes = NotesApp()
-    result = notes.get_note(0)
+def test_get_note_index_error(app_without_notes):
+    result = app_without_notes.get_note(0)
     assert result == "Index out of range"
 
 
-def test_edit_note():
-    notes = NotesApp()
-    notes.add_note("Test note 1")
-    notes.edit_note(0, "Test note 1 edited")
-    result = notes.get_note(0)
+def test_edit_note(app_with_notes):
+    app_with_notes.edit_note(0, "Test note 1 edited")
+    result = app_with_notes.get_note(0)
     assert result == "Test note 1 edited"
 
 
-def test_edit_note_index_error():
-    notes = NotesApp()
-    result = notes.edit_note(0, "Test note 1 edited")
+def test_edit_note_index_error(app_without_notes):
+    result = app_without_notes.edit_note(0, "Test note 1 edited")
     assert result == "Index out of range"

7df93778c96d45dc743fa24a56c94e5d868ae5f3
Ashutosh Krishna <ashutoshbritish@gmail.com>
2023-02-14 21:37:33 +0530

    Fix Testcase and Add docstring

--- test_notes_app.py
+++ test_notes_app.py
@@ -5,24 +5,25 @@ from notes_app import NotesApp
 
 @pytest.fixture
 def app_without_notes():
+    """Returns an instance of the NotesApp class with zero notes."""
     app = NotesApp()
     return app
 
 
 @pytest.fixture
 def app_with_notes():
+    """Returns an instance of the NotesApp class with two notes."""
     app = NotesApp()
     app.add_note("Test note 1")
     app.add_note("Test note 2")
     return app
 
 
-def test_add_note():
-    notes = NotesApp()
-    result = notes.add_note("Test note 1")
+def test_add_note(app_without_notes):
+    result = app_without_notes.add_note("Test note 1")
     assert result == "Note added successfully"
-    assert len(notes.notes_list) == 1
-    assert notes.notes_list[0].content == "Test note 1"
+    assert len(app_without_notes.notes_list) == 1
+    assert app_without_notes.notes_list[0].content == "Test note 1"
 
 
 def test_get_note(app_with_notes):
